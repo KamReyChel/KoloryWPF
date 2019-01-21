911fcd2389ef037905f36a6609292890498ec93b
Rychlak_Kam <rychlak.kam@gmail.com>
2019-01-21 16:06:16 +0100

    Dodaj pliki projektów.

---
+++ KoloryWPF/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KoloryWPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool flaga = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            (OknoGlowne.Height, OknoGlowne.Width) = Ustawienia.CzytajOkno();
+
+            Color kolor = Ustawienia.CzytajKolor();
+            rectangle.Fill = new SolidColorBrush(kolor);
+            
+            
+            SliderR.Value = kolor.R;
+            SliderG.Value = kolor.G;
+            SliderB.Value = kolor.B;
+            flaga = true;
+        }
+
+        private Color KolorProst
+        {
+            get
+            {
+                return (rectangle.Fill as SolidColorBrush).Color;
+            }
+            set
+            {
+                (rectangle.Fill as SolidColorBrush).Color = value;
+            }
+        }
+
+
+
+        private void SliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (flaga)
+            {
+                Color kolor = Color.FromRgb((byte)SliderR.Value, (byte)SliderG.Value, (byte)SliderB.Value);
+                KolorProst = kolor;
+            }
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape) Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            
+            Ustawienia.Zapisz(KolorProst, OknoGlowne.ActualHeight, OknoGlowne.ActualWidth);
+        }
+    }
+}
---
+++ KoloryWPF/Ustawienia.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using KoloryWPF.Properties;
+using System.Windows;
+
+namespace KoloryWPF
+{
+    public static class Ustawienia
+    {
+        public static Color CzytajKolor()
+        {
+            Settings ustawienia = Settings.Default;
+            Color kolor = new Color()
+            {
+                A = 255,
+                R = ustawienia.R,
+                G = ustawienia.G,
+                B = ustawienia.B
+            };
+            return kolor;
+        }
+
+        public static (double H, double W) CzytajOkno()
+        {
+            Settings ustawienia = Settings.Default;
+            double H = ustawienia.H;
+            double W = ustawienia.W;
+            return (H, W);
+        }
+
+        public static void Zapisz(Color kolor, double H, double W)
+        {
+            Settings ustawienia = Settings.Default;
+
+            ustawienia.R = kolor.R;
+            ustawienia.G = kolor.G;
+            ustawienia.B = kolor.B;
+
+            ustawienia.H = H;
+            ustawienia.W = W;
+
+            ustawienia.Save();
+        }
+    }
+}
